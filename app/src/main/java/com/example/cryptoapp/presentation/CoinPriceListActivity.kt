38e0c0a4bd0983840ebbd020623d45777429ab5b
YungDenn <denn.033@mail.ru>
2022-12-11 19:23:51 +0500

    Git init

---
+++ app/src/main/java/com/example/cryptoapp/presentation/CoinPriceListActivity.kt
@@ -0,0 +1,34 @@
+package com.example.cryptoapp.presentation
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
+import com.example.cryptoapp.R
+import com.example.cryptoapp.domain.CoinInfo
+import com.example.cryptoapp.presentation.adapters.CoinInfoAdapter
+import kotlinx.android.synthetic.main.activity_coin_prce_list.*
+
+class CoinPriceListActivity : AppCompatActivity() {
+
+    private lateinit var viewModel: CoinViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_coin_prce_list)
+        val adapter = CoinInfoAdapter(this)
+        adapter.onCoinClickListener = object : CoinInfoAdapter.OnCoinClickListener {
+            override fun onCoinClick(coinPriceInfo: CoinInfo) {
+                val intent = CoinDetailActivity.newIntent(
+                    this@CoinPriceListActivity,
+                    coinPriceInfo.fromSymbol
+                )
+                startActivity(intent)
+            }
+        }
+        rvCoinPriceList.adapter = adapter
+        viewModel = ViewModelProvider(this)[CoinViewModel::class.java]
+        viewModel.coinInfoList.observe(this) {
+            adapter.coinInfoList = it
+        }
+    }
+}
