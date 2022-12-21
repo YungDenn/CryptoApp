6448df8e400bc37804c2c3768b9f0ead37def2ae
YungDenn <denn.033@mail.ru>
2022-12-21 21:12:03 +0500

    Create factory of workers and inject parameters in worker

--- app/src/main/java/com/example/cryptoapp/data/workers/RefreshDataWorker.kt
+++ app/src/main/java/com/example/cryptoapp/data/workers/RefreshDataWorker.kt
@@ -7,6 +7,7 @@ import com.example.cryptoapp.data.database.CoinInfoDao
 import com.example.cryptoapp.data.mapper.CoinMapper
 import com.example.cryptoapp.data.network.ApiService
 import kotlinx.coroutines.delay
+import javax.inject.Inject
 
 class RefreshDataWorker(
     context: Context,
@@ -37,6 +38,23 @@ class RefreshDataWorker(
 
         fun makeRequest(): OneTimeWorkRequest{
             return OneTimeWorkRequestBuilder<RefreshDataWorker>().build()
+        }
+    }
+
+
+    class Factory @Inject constructor(
+        private val coinInfoDao: CoinInfoDao,
+        private val apiService: ApiService,
+        private val mapper: CoinMapper
+    ): ChildWorkerFactory{
+
+        override fun create(
+            context: Context,
+            workerParameters: WorkerParameters
+        ): ListenableWorker {
+            return RefreshDataWorker(
+                context, workerParameters, coinInfoDao, apiService, mapper
+            )
         }
     }
 
--- app/src/main/java/com/example/cryptoapp/presentation/CoinApp.kt
+++ app/src/main/java/com/example/cryptoapp/presentation/CoinApp.kt
@@ -2,14 +2,14 @@ package com.example.cryptoapp.presentation
 
 import android.app.Application
 import androidx.work.Configuration
-import com.example.cryptoapp.data.workers.RefreshDataWorkerFactory
+import com.example.cryptoapp.data.workers.CoinWorkerWorkerFactory
 import com.example.cryptoapp.di.DaggerApplicationComponent
 import javax.inject.Inject
 
 class CoinApp: Application(), Configuration.Provider {
 
     @Inject
-    lateinit var workerFactory: RefreshDataWorkerFactory
+    lateinit var workerFactory: CoinWorkerWorkerFactory
 
     val component by lazy {
         DaggerApplicationComponent.factory().create(this)
