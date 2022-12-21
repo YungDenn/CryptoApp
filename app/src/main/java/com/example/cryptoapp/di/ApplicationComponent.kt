c21d35c7d57e5fbe6470af7f6bef0bcf7cc649f6
YungDenn <denn.033@mail.ru>
2022-12-21 15:49:11 +0500

    Inject into Worker Factory

--- app/src/main/java/com/example/cryptoapp/di/ApplicationComponent.kt
+++ app/src/main/java/com/example/cryptoapp/di/ApplicationComponent.kt
@@ -1,11 +1,13 @@
 package com.example.cryptoapp.di
 
 import android.app.Application
+import com.example.cryptoapp.presentation.CoinApp
 import com.example.cryptoapp.presentation.CoinDetailFragment
 import com.example.cryptoapp.presentation.CoinPriceListActivity
 import dagger.BindsInstance
 import dagger.Component
 
+@ApplicationScope
 @Component(modules = [DataModule::class, ViewModelModule::class])
 interface ApplicationComponent {
 
@@ -13,7 +15,9 @@ interface ApplicationComponent {
 
     fun inject(fragment: CoinDetailFragment)
 
-    @dagger.Component.Factory
+    fun inject (application: CoinApp)
+
+    @Component.Factory
     interface Factory{
         fun create(@BindsInstance application: Application): ApplicationComponent
     }
